1ea127eec98bdcab11003f87b12687121e4b2480
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-01-14 21:03:42 +0400

    Initial

---
+++ src/test/java/com/processing/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processing.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

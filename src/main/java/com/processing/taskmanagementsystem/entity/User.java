deee863c4cd03ceb7cd8cda657a8fc41a763a8f3
artur.tomeyan <artur.tomeyan@ucom.am>
2024-01-15 17:18:26 +0400

    Added User entity and logger object.

--- src/main/java/com/processing/taskmanagementsystem/entity/User.java
+++ src/main/java/com/processing/taskmanagementsystem/entity/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.UUID;
 
@@ -21,4 +24,43 @@ public class User {
     private String username;
 
     private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return new EqualsBuilder()
+                .append(uuid, user.uuid)
+                .append(firstName, user.firstName)
+                .append(lastName, user.lastName)
+                .append(username, user.username)
+                .append(password, user.password)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(uuid)
+                .append(firstName)
+                .append(lastName)
+                .append(username)
+                .append(password)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("uuid", uuid)
+                .append("firstName", firstName)
+                .append("lastName", lastName)
+                .append("username", username)
+                .append("password", password)
+                .toString();
+    }
 }
--- src/main/java/com/processing/taskmanagementsystem/service/impl/TaskServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/TaskServiceImpl.java
@@ -1,6 +1,14 @@
 package com.processing.taskmanagementsystem.service.impl;
 
+import ch.qos.logback.classic.Logger;
 import com.processing.taskmanagementsystem.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class TaskServiceImpl implements TaskService {
+
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(TaskService.class);
 }

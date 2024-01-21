e2414fc3c7542763d5416d124c46a425c7bb9a34
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-01-22 00:12:48 +0400

    Added Task Dtos.

---
+++ src/main/java/com/processing/taskmanagementsystem/dto/TaskRequestDto.java
@@ -0,0 +1,21 @@
+package com.processing.taskmanagementsystem.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class TaskRequestDto implements Serializable {
+    private UUID uuid;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private String priority;
+    private String status;
+}

8ee5221bea3c0b8173a193ce4e1759a72830d1b2
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-01-22 00:13:06 +0400

    Added service methods.

--- src/main/java/com/processing/taskmanagementsystem/service/TaskService.java
+++ src/main/java/com/processing/taskmanagementsystem/service/TaskService.java
@@ -1,4 +1,16 @@
 package com.processing.taskmanagementsystem.service;
 
+import com.processing.taskmanagementsystem.dto.TaskRequestDto;
+import com.processing.taskmanagementsystem.dto.TaskResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface TaskService {
+
+    TaskResponseDto createTask(TaskRequestDto taskRequestDto);
+    TaskResponseDto updateTask(TaskRequestDto taskRequestDto);
+    TaskResponseDto getTaskById(UUID uuid);
+    List<TaskResponseDto> getAllTasks();
+    void deleteTask(UUID uuid);
 }
--- src/main/java/com/processing/taskmanagementsystem/service/impl/TaskServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/TaskServiceImpl.java
@@ -1,11 +1,16 @@
 package com.processing.taskmanagementsystem.service.impl;
 
 import ch.qos.logback.classic.Logger;
+import com.processing.taskmanagementsystem.dto.TaskRequestDto;
+import com.processing.taskmanagementsystem.dto.TaskResponseDto;
 import com.processing.taskmanagementsystem.repository.TaskRepository;
 import com.processing.taskmanagementsystem.service.TaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -19,4 +24,28 @@ public class TaskServiceImpl implements TaskService {
     }
 
 
+    @Override
+    public TaskResponseDto createTask(TaskRequestDto taskRequestDto) {
+        return null;
+    }
+
+    @Override
+    public TaskResponseDto updateTask(TaskRequestDto taskRequestDto) {
+        return null;
+    }
+
+    @Override
+    public TaskResponseDto getTaskById(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public List<TaskResponseDto> getAllTasks() {
+        return null;
+    }
+
+    @Override
+    public void deleteTask(UUID uuid) {
+
+    }
 }

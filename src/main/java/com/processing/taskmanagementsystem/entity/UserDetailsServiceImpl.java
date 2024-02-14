e54c6fdebbe881508ac232f641ce991dffb04d9f
artur.tomeyan <artur.tomeyan@ucom.am>
2024-02-14 17:46:41 +0400

    Implemented user details service.

---
+++ src/main/java/com/processing/taskmanagementsystem/entity/UserDetailsServiceImpl.java
@@ -0,0 +1,42 @@
+package com.processing.taskmanagementsystem.entity;
+
+import com.processing.taskmanagementsystem.exception.NotFoundException;
+import com.processing.taskmanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findUserByUsername(username).orElseThrow(() ->
+                new NotFoundException(String.format("User does not found, username: %s", username)));
+
+        Set<Role> roles = Collections.singleton(user.getUserRoles().stream().iterator().next().getRole());
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getRole()));
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}

c238f63c5653f5540c542fede901f47ea44d1a54
artur.tomeyan <artur.tomeyan@ucom.am>
2024-02-15 17:17:33 +0400

    Implemented jwt provider and filter.

--- src/main/java/com/processing/taskmanagementsystem/config/jwt/JwtFilter.java
+++ src/main/java/com/processing/taskmanagementsystem/config/jwt/JwtFilter.java
@@ -2,16 +2,21 @@ package com.processing.taskmanagementsystem.config.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends OncePerRequestFilter {
 
     private static final String AUTHORIZATION = "Authorization";
     private final JwtProvider jwtProvider;
@@ -23,7 +28,27 @@ public class JwtFilter extends GenericFilterBean {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getToken(request);
 
+        if (token != null && jwtProvider.validateToken(token)) {
+            String username = jwtProvider.getLoginFromJwtToken(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest httpServletRequest) {
+        String bearer = httpServletRequest.getHeader(AUTHORIZATION);
+
+        if (StringUtils.hasText(bearer) && bearer.startsWith("Bearer ")) {
+            return bearer.substring(7);
+        }
+
+        return null;
     }
 }
--- src/main/java/com/processing/taskmanagementsystem/config/jwt/JwtProvider.java
+++ src/main/java/com/processing/taskmanagementsystem/config/jwt/JwtProvider.java
@@ -1,8 +1,80 @@
 package com.processing.taskmanagementsystem.config.jwt;
 
+import com.processing.taskmanagementsystem.entity.User;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
 @Component
+@Slf4j
 public class JwtProvider {
 
+    @Value("${security.jwt.expiration.access-token-time}")
+    private Long accessTokenExpirationTime;
+    @Value("${security.jwt.expiration.refresh-token-time}")
+    private Long refreshTokenExpirationTime;
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    public String accessTokenGenerator(User user) {
+        return generateToken(user, accessTokenExpirationTime);
+    }
+
+    public String refreshTokenGenerator(User user) {
+        return generateToken(user, refreshTokenExpirationTime);
+    }
+
+    public String getUsernameFromJwt(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String getLoginFromJwtToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("Token expired.");
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token.");
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature.");
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty.");
+        }
+
+        return false;
+    }
+
+    private String generateToken(User user, Long expiration) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Claims claims = Jwts.claims().setSubject(user.getUsername());
+        claims.put("firstName", user.getFirstName());
+        claims.put("lastName", user.getLastName());
+        claims.put("authorities", authentication.getAuthorities());
+        Date date = new Date();
+        Date validate = new Date(date.getTime() + expiration);
+
+        Key key = new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKey), SignatureAlgorithm.RS256.getJcaName());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(validate)
+                .signWith(SignatureAlgorithm.RS256, key)
+                .compact();
+    }
 }

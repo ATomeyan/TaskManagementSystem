5de28a57d0796e68ed202593b52bbcdfc1145acf
artur.tomeyan <artur.tomeyan@ucom.am>
2024-02-15 17:18:27 +0400

    Added validator.

--- src/main/java/com/processing/taskmanagementsystem/dto/authentication/UserRegistration.java
+++ src/main/java/com/processing/taskmanagementsystem/dto/authentication/UserRegistration.java
@@ -1,5 +1,9 @@
 package com.processing.taskmanagementsystem.dto.authentication;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,11 +14,21 @@ import java.util.UUID;
 @Getter
 @Setter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserRegistration implements Serializable {
 
     private UUID uuid;
+    @NotNull
+    @Size(max = 255)
     private String firstName;
+    @NotNull
+    @Size(max = 255)
     private String lastName;
+    @NotNull
+    @Size(max = 255)
     private String username;
+    @NotNull
+    @Pattern(message = "Password is not valid", regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,12}$")
+    @Size(min = 8, max = 12)
     private String password;
 }

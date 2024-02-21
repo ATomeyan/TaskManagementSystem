1ea62e5798461b2ee5c6a6a843616b5f660b2b0d
artur.tomeyan <artur.tomeyan@ucom.am>
2024-02-21 17:15:18 +0400

    Implemented create task method.

--- src/main/java/com/processing/taskmanagementsystem/service/impl/TaskServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/TaskServiceImpl.java
@@ -3,13 +3,19 @@ package com.processing.taskmanagementsystem.service.impl;
 import ch.qos.logback.classic.Logger;
 import com.processing.taskmanagementsystem.dto.task.TaskRequestDto;
 import com.processing.taskmanagementsystem.dto.task.TaskResponseDto;
+import com.processing.taskmanagementsystem.dto.user.UserResponseDto;
 import com.processing.taskmanagementsystem.entity.Task;
+import com.processing.taskmanagementsystem.exception.AssignedException;
+import com.processing.taskmanagementsystem.exception.InvalidObjectException;
+import com.processing.taskmanagementsystem.exception.NotFoundException;
 import com.processing.taskmanagementsystem.mapper.TaskMapper;
 import com.processing.taskmanagementsystem.repository.TaskRepository;
 import com.processing.taskmanagementsystem.service.TaskService;
+import com.processing.taskmanagementsystem.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,18 +26,41 @@ public class TaskServiceImpl implements TaskService {
 
     private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(TaskService.class);
     private final TaskRepository taskRepository;
+    private final UserService userService;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, UserService userService) {
         this.taskRepository = taskRepository;
+        this.userService = userService;
     }
 
     @Override
-    public void createTask(TaskRequestDto taskRequestDto) {
+    @Transactional
+    public TaskResponseDto createTask(TaskRequestDto taskRequestDto) {
 
-        Task task = TaskMapper.mapRequestDtoToEntity(taskRequestDto);
+        if (taskRequestDto == null) {
+            LOGGER.error("Task request is null.");
+            throw new InvalidObjectException("Task request is null.");
+        }
 
-        taskRepository.save(task);
+        String username = taskRequestDto.getUserRequestDto().getUsername();
+        UserResponseDto byUsername = userService.findByUsername(username);
+
+        if (byUsername == null) {
+            LOGGER.error("The user by username was not found.");
+            throw new NotFoundException(String.format("The user by username was not found %s:", username));
+        }
+
+        if (byUsername.getTaskResponse() != null) {
+            LOGGER.error("The task already assigned to this user.");
+            throw new AssignedException(String.format("The task already assigned to this username %s:", username));
+        }
+
+        Task createdTask = TaskMapper.mapRequestDtoToEntity(taskRequestDto);
+        taskRepository.save(createdTask);
+
         LOGGER.info("Task created.");
+
+        return TaskMapper.mapEntityToResponseDto(createdTask);
     }
 
     @Override

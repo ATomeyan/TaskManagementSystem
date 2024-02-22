ac2f3fdf2cdfebf292938cb85093ec8ebcaac5ed
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-02-23 00:11:58 +0400

    Added exception handling.

--- src/main/java/com/processing/taskmanagementsystem/service/impl/RoleServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/RoleServiceImpl.java
@@ -1,20 +1,37 @@
 package com.processing.taskmanagementsystem.service.impl;
 
+import ch.qos.logback.classic.Logger;
 import com.processing.taskmanagementsystem.entity.Role;
+import com.processing.taskmanagementsystem.exception.EmptyException;
+import com.processing.taskmanagementsystem.exception.InvalidObjectException;
 import com.processing.taskmanagementsystem.repository.RoleRepository;
 import com.processing.taskmanagementsystem.service.RoleService;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(RoleService.class);
     private final RoleRepository roleRepository;
 
     public RoleServiceImpl(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
 
-    public Role getRoleByName(String role) {
-        return roleRepository.findRoleByName(role);
+    public Role getRoleByName(String roleName) {
+        if (roleName.isBlank()) {
+            LOGGER.error("Role {} is invalid.", roleName);
+            throw new InvalidObjectException(String.format("Role {} is invalid. %s", roleName));
+        }
+
+        Role role = roleRepository.findRoleByName(roleName);
+
+        if (role == null) {
+            LOGGER.error("Role object {} are empty.", role);
+            throw new EmptyException(String.format("Role object {} are empty. %s", role));
+        }
+
+        return role;
     }
 }

49d7a26e228d5d1d2c8c478b38879b09e60306c1
artur.tomeyan <artur.tomeyan@ucom.am>
2024-02-22 17:12:03 +0400

    Changed id type.

--- src/main/java/com/processing/taskmanagementsystem/repository/RoleRepository.java
+++ src/main/java/com/processing/taskmanagementsystem/repository/RoleRepository.java
@@ -4,8 +4,7 @@ import com.processing.taskmanagementsystem.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface RoleRepository extends JpaRepository<Role, UUID> {
+public interface RoleRepository extends JpaRepository<Role, String> {
+    Role findRoleByName(String name);
 }
--- src/main/java/com/processing/taskmanagementsystem/repository/TaskRepository.java
+++ src/main/java/com/processing/taskmanagementsystem/repository/TaskRepository.java
@@ -4,8 +4,6 @@ import com.processing.taskmanagementsystem.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface TaskRepository extends JpaRepository<Task, UUID> {
+public interface TaskRepository extends JpaRepository<Task, String> {
 }

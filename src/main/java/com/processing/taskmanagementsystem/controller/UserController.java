20b7a3668ae3a131b45d7c14bb23dc670b568cff
artur.tomeyan <artur.tomeyan@ucom.am>
2024-02-22 17:14:53 +0400

    Removed redundant import.

--- src/main/java/com/processing/taskmanagementsystem/controller/UserController.java
+++ src/main/java/com/processing/taskmanagementsystem/controller/UserController.java
@@ -6,7 +6,6 @@ import com.processing.taskmanagementsystem.dto.authentication.UserAuthentication
 import com.processing.taskmanagementsystem.dto.authentication.UserRegistration;
 import com.processing.taskmanagementsystem.service.UserAuthenticationService;
 import jakarta.validation.Valid;
-import org.hibernate.validator.constraints.ParameterScriptAssert;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;

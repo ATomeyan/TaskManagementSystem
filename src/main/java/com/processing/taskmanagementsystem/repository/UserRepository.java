6443a7221f1b27f6dd8dd9939cafac5b13c44442
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-02-25 23:18:03 +0400

    Provided delete user query.

--- src/main/java/com/processing/taskmanagementsystem/repository/UserRepository.java
+++ src/main/java/com/processing/taskmanagementsystem/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.processing.taskmanagementsystem.repository;
 
 import com.processing.taskmanagementsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +14,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findUserByUsername(String username);
+
+    @Query("update User u set u.enabled=false where u.uuid=:id and u.enabled=true")
+    @Modifying
+    @Override
+    void deleteById(@Param("id") String id);
 }

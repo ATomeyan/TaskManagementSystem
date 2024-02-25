567714b6c16a97fc2c9dcbf673146b8888f52bdc
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-02-25 23:17:20 +0400

    Implemented get all and remove user service methods.

--- src/main/java/com/processing/taskmanagementsystem/service/UserService.java
+++ src/main/java/com/processing/taskmanagementsystem/service/UserService.java
@@ -2,7 +2,17 @@ package com.processing.taskmanagementsystem.service;
 
 import com.processing.taskmanagementsystem.dto.user.UserResponseDto;
 
+import java.util.List;
+
 public interface UserService {
 
+    List<UserResponseDto> getAllUsers();
+
+    List<UserResponseDto> getAllUsersWithoutTask();
+
+    List<UserResponseDto> getAllUsersWithTask();
+
     UserResponseDto findByUsername(String username);
+
+    boolean deleteUser(String uuid);
 }
--- src/main/java/com/processing/taskmanagementsystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/UserServiceImpl.java
@@ -3,12 +3,14 @@ package com.processing.taskmanagementsystem.service.impl;
 import ch.qos.logback.classic.Logger;
 import com.processing.taskmanagementsystem.dto.user.UserResponseDto;
 import com.processing.taskmanagementsystem.entity.User;
+import com.processing.taskmanagementsystem.exception.NotFoundException;
 import com.processing.taskmanagementsystem.mapper.UserMapper;
 import com.processing.taskmanagementsystem.repository.UserRepository;
 import com.processing.taskmanagementsystem.service.UserService;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +21,65 @@ public class UserServiceImpl implements UserService {
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserResponseDto> getAllUsers() {
+        List<User> allUsers = userRepository.findAll();
+
+        if (allUsers.isEmpty()) {
+            LOGGER.error("User not found.");
+            throw new NotFoundException("User not found.");
+        }
+
+        return allUsers.stream().map(UserMapper::mapEntityToUserResponse).toList();
+    }
+
+    @Override
+    public List<UserResponseDto> getAllUsersWithoutTask() {
+
+        List<User> allUsers = userRepository.findAll();
+
+        if (allUsers.isEmpty()) {
+            LOGGER.error("User not found.");
+            throw new NotFoundException("User not found.");
+        }
+
+        List<UserResponseDto> usersWithoutTask = allUsers
+                .stream()
+                .filter(user -> user.getTask() == null)
+                .map(UserMapper::mapEntityToUserResponse)
+                .toList();
+
+        if (usersWithoutTask.isEmpty()) {
+            LOGGER.error("User without task was not found.");
+            throw new NotFoundException("User without task was not found.");
+        }
+
+        return usersWithoutTask;
+    }
+
+    @Override
+    public List<UserResponseDto> getAllUsersWithTask() {
+        List<User> allUsers = userRepository.findAll();
+
+        if (allUsers.isEmpty()) {
+            LOGGER.error("User not found.");
+            throw new NotFoundException("User not found.");
+        }
+
+        List<UserResponseDto> usersWithTask = allUsers
+                .stream()
+                .filter(u -> u.getTask() != null)
+                .map(UserMapper::mapEntityToUserResponse)
+                .toList();
+
+        if (usersWithTask.isEmpty()) {
+            LOGGER.error("User with assigned task was not found.");
+            throw new NotFoundException("User with assigned task was not found.");
+        }
+
+        return usersWithTask;
     }
 
     @Override
@@ -36,5 +97,20 @@ public class UserServiceImpl implements UserService {
         }
 
         return userResponseDto;
+    }
+
+    @Override
+    public boolean deleteUser(String uuid) {
+
+        Optional<User> byId = userRepository.findById(uuid);
+
+        if (byId.isPresent()) {
+            userRepository.deleteById(uuid);
+            LOGGER.info("User by {} uid was deleted.", uuid);
+            return true;
+        } else {
+            LOGGER.error("User by {} uid was not found.", uuid);
+            throw new NotFoundException(String.format("User by {} uid was not found. %s", uuid));
+        }
     }
 }

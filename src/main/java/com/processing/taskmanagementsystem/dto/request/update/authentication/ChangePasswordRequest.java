f5d54dd0eae88ac1f2081c7d25b2df0f827fe08a
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-02-28 00:24:03 +0400

    Moved to update package.

---
+++ src/main/java/com/processing/taskmanagementsystem/dto/request/update/authentication/ChangePasswordRequest.java
@@ -0,0 +1,25 @@
+package com.processing.taskmanagementsystem.dto.request.update.authentication;
+
+import com.processing.taskmanagementsystem.utils.ValidRegexp;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+public class ChangePasswordRequest implements Serializable {
+
+    private String uuid;
+    @NotBlank
+    private String oldPassword;
+    @NotBlank
+    @Pattern(message = "Password is not valid", regexp = ValidRegexp.password)
+    @Size(min = 8, max = 12)
+    private String newPassword;
+}

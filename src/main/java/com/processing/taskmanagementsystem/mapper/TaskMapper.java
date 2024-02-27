60d8301de51d279dcfd60f4b92d7d036a41dda29
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-02-28 00:25:08 +0400

    Changed logic and added update method.

--- src/main/java/com/processing/taskmanagementsystem/mapper/TaskMapper.java
+++ src/main/java/com/processing/taskmanagementsystem/mapper/TaskMapper.java
@@ -1,11 +1,13 @@
 package com.processing.taskmanagementsystem.mapper;
 
 import com.processing.taskmanagementsystem.dto.request.task.TaskRequestDto;
+import com.processing.taskmanagementsystem.dto.request.update.task.TaskUpdateRequestDto;
 import com.processing.taskmanagementsystem.dto.response.task.TaskResponseDto;
 import com.processing.taskmanagementsystem.entity.Task;
 import com.processing.taskmanagementsystem.entity.TaskUser;
 import com.processing.taskmanagementsystem.entity.User;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,14 +17,14 @@ public class TaskMapper {
     }
 
     public static Task mapRequestDtoToEntity(User user, TaskRequestDto taskRequestDto) {
-        Task task = new Task();
 
-        task.setUuid(taskRequestDto.getUuid());
-        task.setTitle(taskRequestDto.getTitle());
-        task.setDescription(taskRequestDto.getDescription());
-        task.setDueDate(taskRequestDto.getDueDate());
-        task.setPriority(taskRequestDto.getPriority());
-        task.setStatus(taskRequestDto.getStatus());
+        Task task = setTaskEntity(taskRequestDto.getUuid(),
+                taskRequestDto.getTitle(),
+                taskRequestDto.getDescription(),
+                taskRequestDto.getDueDate(),
+                taskRequestDto.getPriority(),
+                taskRequestDto.getStatus());
+
         task.setCreated(LocalDateTime.now());
 
         TaskUser taskUser = TaskUserMapper.mapRequestToEntity(task, user);
@@ -42,5 +44,31 @@ public class TaskMapper {
                 .status(task.getStatus())
                 .userResponseDto(UserMapper.mapEntityToUserResponse(task.getTaskUsers().stream().iterator().next().getUser()))
                 .build();
+    }
+
+    public static Task mapUpdateRequestToEntity(TaskUpdateRequestDto taskUpdateRequestDto) {
+        Task task = setTaskEntity(taskUpdateRequestDto.getUuid(),
+                taskUpdateRequestDto.getTitle(),
+                taskUpdateRequestDto.getDescription(),
+                taskUpdateRequestDto.getDueDate(),
+                taskUpdateRequestDto.getPriority(),
+                taskUpdateRequestDto.getStatus());
+
+        task.setUpdated(LocalDateTime.now());
+
+        return task;
+    }
+
+    private static Task setTaskEntity(String uuid, String title, String description, LocalDate dueDate, String priority, String status) {
+        Task task = new Task();
+
+        task.setUuid(uuid);
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setDueDate(dueDate);
+        task.setPriority(priority);
+        task.setStatus(status);
+
+        return task;
     }
 }

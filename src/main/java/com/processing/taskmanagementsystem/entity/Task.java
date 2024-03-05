e8483e6134082beed9c6fd7019d15c1452dfa436
artur.tomeyan <artur.tomeyan@ucom.am>
2024-03-05 17:27:54 +0400

    Added builder annotation.

--- src/main/java/com/processing/taskmanagementsystem/entity/Task.java
+++ src/main/java/com/processing/taskmanagementsystem/entity/Task.java
@@ -1,10 +1,7 @@
 package com.processing.taskmanagementsystem.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -16,6 +13,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "tb_task")
 public class Task extends BaseEntity {

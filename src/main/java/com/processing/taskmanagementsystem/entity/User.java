97c433595095a10797b3cf9880266067d94a2296
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-03-10 23:53:35 +0400

    Initial.

--- src/main/java/com/processing/taskmanagementsystem/entity/User.java
+++ src/main/java/com/processing/taskmanagementsystem/entity/User.java
@@ -1,10 +1,7 @@
 package com.processing.taskmanagementsystem.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;

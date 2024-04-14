78926f6eafd2908a5ff7583d7cfbc2cd47565eab
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-04-15 00:12:32 +0400

    Added validation exception handler.

--- src/main/java/com/processing/taskmanagementsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/processing/taskmanagementsystem/exception/GlobalExceptionHandler.java
@@ -3,97 +3,123 @@ package com.processing.taskmanagementsystem.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@RestControllerAdvice
-public class AdviserController extends ResponseEntityExceptionHandler {
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpServletRequest httpServletRequest) {
+        BindingResult result = ex.getBindingResult();
+        String errorMsg = result.getFieldErrors().stream().iterator().next().getDefaultMessage();
+
+        ExceptionResponse response = new ExceptionResponse(
+                UUID.randomUUID(),
+                BAD_REQUEST.getReasonPhrase(),
+                errorMsg,
+                BAD_REQUEST.value(),
+                BAD_REQUEST.name(),
+                httpServletRequest.getRequestURI(),
+                httpServletRequest.getMethod(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(UserAssignedToTaskException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(UserAssignedToTaskException e, HttpServletRequest httpServletRequest) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 UUID.randomUUID(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                BAD_REQUEST.getReasonPhrase(),
                 e.getMessage(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.name(),
+                BAD_REQUEST.value(),
+                BAD_REQUEST.name(),
                 httpServletRequest.getRequestURI(),
                 httpServletRequest.getMethod(),
                 LocalDateTime.now()
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, BAD_REQUEST);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(UserAlreadyExistException e, HttpServletRequest httpServletRequest) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 UUID.randomUUID(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                BAD_REQUEST.getReasonPhrase(),
                 e.getMessage(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.name(),
+                BAD_REQUEST.value(),
+                BAD_REQUEST.name(),
                 httpServletRequest.getRequestURI(),
                 httpServletRequest.getMethod(),
                 LocalDateTime.now()
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, BAD_REQUEST);
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException e, HttpServletRequest httpServletRequest) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 UUID.randomUUID(),
-                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                NOT_FOUND.getReasonPhrase(),
                 e.getMessage(),
-                HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND.name(),
+                NOT_FOUND.value(),
+                NOT_FOUND.name(),
                 httpServletRequest.getRequestURI(),
                 httpServletRequest.getMethod(),
                 LocalDateTime.now()
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(exceptionResponse, NOT_FOUND);
     }
 
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(InvalidObjectException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(InvalidObjectException e, HttpServletRequest httpServletRequest) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 UUID.randomUUID(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                BAD_REQUEST.getReasonPhrase(),
                 e.getMessage(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.name(),
+                BAD_REQUEST.value(),
+                BAD_REQUEST.name(),
                 httpServletRequest.getRequestURI(),
                 httpServletRequest.getMethod(),
                 LocalDateTime.now()
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, BAD_REQUEST);
     }
 
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(EmptyException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(EmptyException e, HttpServletRequest httpServletRequest) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 UUID.randomUUID(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                BAD_REQUEST.getReasonPhrase(),
                 e.getMessage(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.name(),
+                BAD_REQUEST.value(),
+                BAD_REQUEST.name(),
                 httpServletRequest.getRequestURI(),
                 httpServletRequest.getMethod(),
                 LocalDateTime.now()
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, BAD_REQUEST);
     }
 }

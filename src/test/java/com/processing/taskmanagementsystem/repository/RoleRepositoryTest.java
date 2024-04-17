4ce3d5a87a1f80923313ab85641b7ebe49766450
Artur Tomeyan <artur.tomeyan@ucom.am>
2024-04-17 17:12:56 +0400

    Implemented role repository unit test.

---
+++ src/test/java/com/processing/taskmanagementsystem/repository/RoleRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.processing.taskmanagementsystem.repository;
+
+import com.processing.taskmanagementsystem.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class RoleRepositoryTest {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    void findRoleByName() {
+
+        Role admin = roleRepository.findRoleByName("ADMIN");
+
+        assertThat(admin).isNotNull();
+    }
+}

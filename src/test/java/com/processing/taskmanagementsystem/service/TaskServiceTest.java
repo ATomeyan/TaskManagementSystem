83c6c57e7996e2eaaefbc793988f68c063bf0225
Artur Tomeyan <artur.tomeyan@ucom.am>
2024-04-17 17:11:30 +0400

    Implemented task service unit test.

--- src/test/java/com/processing/taskmanagementsystem/service/TaskServiceTest.java
+++ src/test/java/com/processing/taskmanagementsystem/service/TaskServiceTest.java
@@ -1,4 +1,66 @@
 package com.processing.taskmanagementsystem.service;
 
+import com.processing.taskmanagementsystem.dto.request.task.TaskRequestDto;
+import com.processing.taskmanagementsystem.dto.response.task.TaskResponseDto;
+import com.processing.taskmanagementsystem.entity.Task;
+import com.processing.taskmanagementsystem.repository.TaskRepository;
+import com.processing.taskmanagementsystem.service.impl.TaskServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class TaskServiceTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+    @InjectMocks
+    private TaskServiceImpl taskService;
+    private Task task;
+    private TaskRequestDto taskRequest;
+
+    @BeforeEach
+    public void init() {
+        task = Task.builder()
+                .title("Button")
+                .description("Create btn.")
+                .dueDate(LocalDate.now())
+                .priority("1")
+                .status("To do")
+                .build();
+
+        taskRequest = TaskRequestDto.builder()
+                .title("Pop up menu")
+                .description("Create pop up menu.")
+                .dueDate(LocalDate.now())
+                .priority("1")
+                .status("To do")
+                .username("tomeyan")
+                .build();
+    }
+
+    @Test
+    void givenTask_whenSaveTask_thenReturnSavedTask() {
+
+        given(taskRepository.findById(task.getUuid())).willReturn(Optional.empty());
+
+        given(taskRepository.save(task)).willReturn(task);
+
+        TaskResponseDto savedTask = taskService.createTask(taskRequest);
+
+        System.out.println(savedTask);
+        assertThat(savedTask).isNotNull();
+    }
 }

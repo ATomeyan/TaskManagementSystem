b52b1f80af843bfd9ebb27bf47120aa39dbfe941
Artur Tomeyan <artur.tomeyan@ucom.am>
2024-04-23 16:52:10 +0400

    Removed query from.

--- src/main/java/com/processing/taskmanagementsystem/repository/TaskRepository.java
+++ src/main/java/com/processing/taskmanagementsystem/repository/TaskRepository.java
@@ -1,15 +1,14 @@
 package com.processing.taskmanagementsystem.repository;
 
+import com.processing.taskmanagementsystem.entity.Status;
 import com.processing.taskmanagementsystem.entity.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, String> {
 
-    @Query("select t from Task t where t.status=:criteria")
-    Page<Task> findTasksByCriteria(String criteria, Pageable pageable);
+    Page<Task> findAllTasksByStatus(Status status, Pageable pageable);
 }

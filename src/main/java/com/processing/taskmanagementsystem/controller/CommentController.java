c2a1d5190c1fab8c30c0ee4ef3fcde16ff1a8462
Artur Tomeyan <artur.tomeyan@ucom.am>
2024-04-26 16:53:12 +0400

    Implemented comment controller.

---
+++ src/main/java/com/processing/taskmanagementsystem/controller/CommentController.java
@@ -0,0 +1,49 @@
+package com.processing.taskmanagementsystem.controller;
+
+import com.processing.taskmanagementsystem.dto.request.comment.CommentRequestDto;
+import com.processing.taskmanagementsystem.dto.request.update.comment.CommentUpdateRequestDto;
+import com.processing.taskmanagementsystem.dto.response.comment.CommentResponseDto;
+import com.processing.taskmanagementsystem.service.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> addComment(@RequestBody @Valid CommentRequestDto commentRequestDto) {
+        CommentResponseDto commentResponseDto = commentService.addComment(commentRequestDto);
+
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/by/{uuid}")
+    public ResponseEntity<CommentResponseDto> getCommentByUid(@PathVariable String uuid) {
+        CommentResponseDto commentById = commentService.getCommentById(uuid);
+
+        return new ResponseEntity<>(commentById, HttpStatus.OK);
+    }
+
+    @PatchMapping
+    public ResponseEntity<CommentResponseDto> updateComment(@RequestBody CommentUpdateRequestDto commentUpdateRequestDto) {
+        CommentResponseDto commentResponseDto = commentService.updateComment(commentUpdateRequestDto);
+
+        return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<Void> deleteComment(@PathVariable String uuid) {
+        commentService.deleteComment(uuid);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}

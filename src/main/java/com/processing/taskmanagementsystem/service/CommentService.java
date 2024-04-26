0082de2f1af8e52e8f024dc1b10c1a863113fe0a
Artur Tomeyan <artur.tomeyan@ucom.am>
2024-04-26 16:54:42 +0400

    Removed get all comments method.

--- src/main/java/com/processing/taskmanagementsystem/service/CommentService.java
+++ src/main/java/com/processing/taskmanagementsystem/service/CommentService.java
@@ -4,8 +4,6 @@ import com.processing.taskmanagementsystem.dto.request.comment.CommentRequestDto
 import com.processing.taskmanagementsystem.dto.request.update.comment.CommentUpdateRequestDto;
 import com.processing.taskmanagementsystem.dto.response.comment.CommentResponseDto;
 
-import java.util.List;
-
 /**
  * @author Artur Tomeyan
  * @date 25.04.2024
@@ -14,9 +12,7 @@ public interface CommentService {
 
     CommentResponseDto addComment(CommentRequestDto commentRequestDto);
 
-    List<CommentResponseDto> getAllComments();
-
-    CommentResponseDto getCommentById(int id);
+    CommentResponseDto getCommentById(String uuid);
 
     CommentResponseDto updateComment(CommentUpdateRequestDto commentUpdateRequestDto);
 

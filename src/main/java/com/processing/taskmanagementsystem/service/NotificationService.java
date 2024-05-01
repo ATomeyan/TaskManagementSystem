37cd64e9508787acf925dac296d7f99e56ea47c8
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-05-02 00:27:06 +0400

    Added new methods in notification service.

--- src/main/java/com/processing/taskmanagementsystem/service/NotificationService.java
+++ src/main/java/com/processing/taskmanagementsystem/service/NotificationService.java
@@ -1,8 +1,18 @@
 package com.processing.taskmanagementsystem.service;
 
+import com.processing.taskmanagementsystem.dto.request.notification.NotificationRequestDto;
+
 /**
  * @author Artur Tomeyan
  * @date 30.04.2024
  */
 public interface NotificationService {
+
+    void generateNotificationByDueDateDeadLine(NotificationRequestDto notificationRequestDto);
+
+    void generateNotificationByAssignedNewTask(NotificationRequestDto notificationRequestDto);
+
+    void generateNotificationByUpdatingTask(NotificationRequestDto notificationRequestDto);
+
+    void deleteNotification(String uuid);
 }
--- src/main/java/com/processing/taskmanagementsystem/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/NotificationServiceImpl.java
@@ -1,6 +1,10 @@
 package com.processing.taskmanagementsystem.service.impl;
 
+import ch.qos.logback.classic.Logger;
+import com.processing.taskmanagementsystem.dto.request.notification.NotificationRequestDto;
+import com.processing.taskmanagementsystem.repository.NotificationRepository;
 import com.processing.taskmanagementsystem.service.NotificationService;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,4 +14,31 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
+
+    private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(NotificationService.class);
+    private final NotificationRepository notificationRepository;
+
+    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public void generateNotificationByDueDateDeadLine(NotificationRequestDto notificationRequestDto) {
+
+    }
+
+    @Override
+    public void generateNotificationByAssignedNewTask(NotificationRequestDto notificationRequestDto) {
+
+    }
+
+    @Override
+    public void generateNotificationByUpdatingTask(NotificationRequestDto notificationRequestDto) {
+
+    }
+
+    @Override
+    public void deleteNotification(String uuid) {
+
+    }
 }

89029abcdfc1f76a53fb83f76b71b8c33ac24f6a
Artur Tomeyan <tomeyan.artur@gmail.com>
2024-05-02 00:26:29 +0400

    Added notification request dto.

---
+++ src/main/java/com/processing/taskmanagementsystem/dto/request/notification/NotificationRequestDto.java
@@ -0,0 +1,26 @@
+package com.processing.taskmanagementsystem.dto.request.notification;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @author Artur Tomeyan
+ * @date 01.05.2024
+ */
+
+@Setter
+@Getter
+@Builder
+public class NotificationRequestDto implements Serializable {
+
+    @NotBlank
+    private String message;
+    @NotBlank
+    private boolean isRead;
+    @NotBlank
+    private String userUid;
+}

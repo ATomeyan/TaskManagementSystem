17fb0c3b936ad18ea82750393fd736304960f4fc
Artur Tomeyan <artur.tomeyan@ucom.am>
2024-05-06 17:02:49 +0400

    Implemented generate notification method

--- src/main/java/com/processing/taskmanagementsystem/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/processing/taskmanagementsystem/service/impl/NotificationServiceImpl.java
@@ -2,7 +2,14 @@ package com.processing.taskmanagementsystem.service.impl;
 
 import ch.qos.logback.classic.Logger;
 import com.processing.taskmanagementsystem.dto.request.notification.NotificationRequestDto;
+import com.processing.taskmanagementsystem.dto.response.notification.NotificationResponse;
+import com.processing.taskmanagementsystem.entity.Notification;
+import com.processing.taskmanagementsystem.entity.User;
+import com.processing.taskmanagementsystem.exception.InvalidObjectException;
+import com.processing.taskmanagementsystem.exception.NotFoundException;
+import com.processing.taskmanagementsystem.mapper.NotificationMapper;
 import com.processing.taskmanagementsystem.repository.NotificationRepository;
+import com.processing.taskmanagementsystem.repository.UserRepository;
 import com.processing.taskmanagementsystem.service.NotificationService;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,19 +25,37 @@ public class NotificationServiceImpl implements NotificationService {
 
     private static final Logger LOGGER = (Logger) LoggerFactory.getLogger(NotificationService.class);
     private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
 
-    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+    public NotificationServiceImpl(NotificationRepository notificationRepository, UserRepository userRepository) {
         this.notificationRepository = notificationRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
-    public void generateNotificationByAssignedNewTask(NotificationRequestDto notificationRequestDto) {
+    public NotificationResponse generateNotificationByAssignedNewTask(NotificationRequestDto notificationRequestDto) {
 
+        if (notificationRequestDto == null) {
+            LOGGER.error("Notification request is null.");
+            throw new InvalidObjectException("Notification request is null.");
+        }
+
+        String userUid = notificationRequestDto.getUserUid();
+
+        User user = userRepository.findById(userUid).orElseThrow(() ->
+                new NotFoundException(String.format("User by uid not found: %s", userUid)));
+
+        Notification notification = NotificationMapper.mapNotificationRequestToResponse(user, notificationRequestDto);
+
+        Notification createdNotification = notificationRepository.save(notification);
+        LOGGER.info("Notification successfully created.");
+
+        return NotificationMapper.mapNotificationToResponse(createdNotification);
     }
 
     @Override
-    public void generateNotificationByUpdatingTask(NotificationRequestDto notificationRequestDto) {
-
+    public NotificationResponse generateNotificationByUpdatingTask(NotificationRequestDto notificationRequestDto) {
+        return null;
     }
 
     @Override
